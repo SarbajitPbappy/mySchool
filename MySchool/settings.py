f6a6d5a69fd5899cdb70bd3757a8177fcba686f4
Bappy6155 <105677054+Bappy6155@users.noreply.github.com>
2023-09-06 20:44:51 +0600

    MySchool

--- MySchool/settings.py
+++ MySchool/settings.py
@@ -122,18 +122,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, images)
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'other_static_directory'),
+    BASE_DIR/'static',
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Media files (user-uploaded content)
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR/'media'
 
 
 # Default primary key field type
